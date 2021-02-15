f7f6a75b9869db512e72b751ee8f852a7d0a5923
WildestDeuce <48850844+WildestDeuce@users.noreply.github.com>
2021-02-15 14:12:32 -0500

    set constants in App() and added Dashboard to components directory

--- crypto-dashboard/src/App.js
+++ crypto-dashboard/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'React';
+import Dashboard from '../components/Dashboard';
 import './App.css';
 
 function App() {
+
+  const [currencies, setCurrencies] = useState([]);
+  const [pair, setPair] = useState('');
+  const [price, setPrice] = useState('0.00');
+  const [pastData, setPastData] = useState({});
+  const ws = useRef(null);
+
+  let first = useRef(false);
+  const url = 'https://api.pro.coinbase.com';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
