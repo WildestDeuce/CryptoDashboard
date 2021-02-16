68e927c518cb90b61cd1da515bec0aab22bda973
WildestDeuce <48850844+WildestDeuce@users.noreply.github.com>
2021-02-15 22:36:05 -0500

    created api call and filter method for filtering currencies

--- crypto-dashboard/src/App.js
+++ crypto-dashboard/src/App.js
@@ -8,10 +8,46 @@ function App() {
   const [pair, setPair] = useState('');
   const [price, setPrice] = useState('0.00');
   const [pastData, setPastData] = useState({});
-  const ws = useRef(null);
+  const webSocket = useRef(null);
 
   let first = useRef(false);
   const url = 'https://api.pro.coinbase.com';
+
+  webSocket.current = new WebSocket('wss://ws-feed.pro.coinbase.com');
+
+  let pairs = [];
+
+  const apiCall = async () => {
+    await fetch(url + '/products')
+      .then((res) => res.json())
+      .then((data) => (pairs = data));
+    console.log('pairs', pairs);
+
+    let filtered = pairs.filter((pair) => {
+      if (pair.quote_currency === 'USD') {
+        return pair;
+      }
+    });
+
+    filtered = filtered.sort((a, b) => {
+      if (a.base_currency < b.base_currency) {
+        return -1;
+      }
+      if (a.base_currency > b.base_currency) {
+        return 1;
+      }
+      return 0;
+    });
+
+    console.log(filtered);
+    setCurrencies(filtered);
+
+    first.current = true;
+  };
+  useEffect(() => {
+    apiCall();
+  }, []);
+
 
   return (
     <div className="App">
