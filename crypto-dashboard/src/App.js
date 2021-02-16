af33122951fcd65c499a66c49e92f28be11e4e3d
WildestDeuce <48850844+WildestDeuce@users.noreply.github.com>
2021-02-15 23:00:44 -0500

    removed Dashboard to check for api data, stopping for tonight

--- crypto-dashboard/src/App.js
+++ crypto-dashboard/src/App.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect, useRef } from 'React';
-import Dashboard from '../components/Dashboard';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
@@ -13,41 +12,41 @@ function App() {
   let first = useRef(false);
   const url = 'https://api.pro.coinbase.com';
 
-  webSocket.current = new WebSocket('wss://ws-feed.pro.coinbase.com');
-
-  let pairs = [];
-
-  const apiCall = async () => {
-    await fetch(url + '/products')
-      .then((res) => res.json())
-      .then((data) => (pairs = data));
-    console.log('pairs', pairs);
-
-    let filtered = pairs.filter((pair) => {
-      if (pair.quote_currency === 'USD') {
-        return pair;
-      }
-    });
-
-    filtered = filtered.sort((a, b) => {
-      if (a.base_currency < b.base_currency) {
-        return -1;
-      }
-      if (a.base_currency > b.base_currency) {
-        return 1;
-      }
-      return 0;
-    });
-
-    console.log(filtered);
-    setCurrencies(filtered);
-
-    first.current = true;
-  };
   useEffect(() => {
-    apiCall();
-  }, []);
+    webSocket.current = new WebSocket('wss://ws-feed.pro.coinbase.com');
 
+    let pairs = [];
+
+    const apiCall = async () => {
+      await fetch(url + '/products')
+        .then((res) => res.json())
+        .then((data) => (pairs = data));
+      console.log('pairs', pairs);
+
+      let filtered = pairs.filter((pair) => {
+        if (pair.quote_currency === 'USD') {
+          return pair;
+        }
+      });
+
+      filtered = filtered.sort((a, b) => {
+        if (a.base_currency < b.base_currency) {
+          return -1;
+        }
+        if (a.base_currency > b.base_currency) {
+          return 1;
+        }
+        return 0;
+      });
+
+      console.log(filtered);
+      setCurrencies(filtered);
+
+      first.current = true;
+    };
+    apiCall();
+    //same as componentDidMount prevents infinite loop
+  }, []);
 
   return (
     <div className="App">
